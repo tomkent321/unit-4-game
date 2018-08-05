ec59972ad6e480aa8d4e6584661cfa2bdbdb26e4
Thomas Hurd <thomashurd@Thomass-MacBook-Pro.local>
2018-08-04 19:07:39 -0600

    First JS and add images

---
+++ assets/javascript/game.js
@@ -0,0 +1,189 @@
+
+
+
+
+
+//list of variables
+
+var arrCrystalWhite = [ "Crystal-1-W", 
+                        "Crystal-2-W",
+                        "Crystal-3-W", 
+                        "Crystal-4-W", 
+                        "Crystal-5-W", 
+                        "Crystal-6-W", 
+                        "Crystal-7-W", 
+                        "Crystal-8-W"
+                        ];
+
+var arrCrystalBlack = [ "Crystal-1-B", 
+                        "Crystal-2-B",
+                        "Crystal-3-B", 
+                        "Crystal-4-B", 
+                        "Crystal-5-B",   
+];
+
+var arrCrystalImage = [];
+var arrCrystalValue = [];
+var arrIndexNum = [];
+var indexNum;
+var wins, losses;
+var targetValue;
+var useArray;
+var blackBack = true;
+
+  
+
+function initialize() {
+
+ //create four crystals and append them to the page
+
+//alternate between white and black background pictures, just thought it would be cool. 
+
+var arrIndexNum = [];
+var arrCrystalImage = [];
+
+for (var i = 0; i < 4; i++) {
+
+        // if last used background is black use white    this could be rewritten to take out the else. 
+    if (blackBack) {
+        
+        // if the index array is empty pick the first random number
+
+        if (arrIndexNum.length === 0) {
+            indexNum = Math.floor((Math.random() * arrCrystalWhite.length));
+            arrIndexNum.push(indexNum);
+        // if the index array has numbers, select an unused index number at random
+        } else {
+
+        do {
+        indexNum = Math.floor((Math.random() * arrCrystalWhite.length));   // took out -1 at the end
+        
+        } while (arrIndexNum.indexOf(indexNum) != -1 );
+        arrIndexNum.push(indexNum);
+    }
+
+        //if the last background was white, use black this time
+    } else {
+
+        if (arrIndexNum.length === 0) {
+            indexNum = Math.floor((Math.random() * arrCrystalBlack.length));
+            arrIndexNum.push(indexNum);
+        } else {
+
+        do {
+        indexNum = Math.floor((Math.random() * arrCrystalBlack.length));
+        
+        } while (arrIndexNum.indexOf(indexNum) != -1 );        
+        arrIndexNum.push(indexNum);
+    }
+    
+    }
+
+//create random computer target and append to the page
+//show wins, losses and current score on page
+}
+    
+// put the random pictures selected into the arrCrystal Image Array
+
+    for (var i =0; i < 4; i++) {
+
+        if (blackBack) {
+            var x = parseInt(arrIndexNum[i]);
+            arrCrystalImage.push(arrCrystalWhite[x]);
+
+        } else {
+            var x = parseInt(arrIndexNum[i]);
+            arrCrystalImage.push(arrCrystalBlack[x]);
+        }
+    }
+
+if(blackBack) {
+    blackBack = false; 
+} else {
+    blackBack = true;
+}
+
+console.log(arrCrystalImage);
+
+}
+
+
+
+function getChoice() {
+    
+    //create on click functions for the crystals
+//grab the value of the crystal that is clicked - maybe use $(this)
+//add the value to the current score and render that to the page
+
+
+}
+
+function scoreChoice() {
+    //if current score is equal to the target number, then we win
+    //alert that the user has won
+    //wins++
+    //call the win page
+    //call a reset function
+
+    //if current score is greater than target score, you lose
+    //alert that they have lost
+    //losses++
+    //reset function
+}
+
+
+function showWin() {
+    // ++wins
+    //show winner splash
+    //play again
+    // call reset
+}
+
+function showLose() {
+    // ++losses
+    // show loser screen
+    // call reset
+
+}
+
+function reset() {
+    //generate a new random number
+    //generate four new crystal values
+    //reset current score to zero
+    //append page to show all changes
+
+}
+
+
+
+initialize();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
